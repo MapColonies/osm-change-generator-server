b8f0ac3a548f379160ee4d18a3649cba533c299b
Daniel Hermon <danielhermonsync@gmail.com>
2021-04-20 11:41:57 +0300

    chore: update server by boilerplate

--- src/serverBuilder.ts
+++ src/serverBuilder.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import { getErrorHandlerMiddleware } from '@map-colonies/error-express-handler';
 import { OpenapiViewerRouter, OpenapiRouterConfig } from '@map-colonies/openapi-express-viewer';
 import { middleware as OpenApiMiddleware } from 'express-openapi-validator';
@@ -41,6 +42,9 @@ export class ServerBuilder {
   }
 
   private registerPreRoutesMiddleware(): void {
+    if (this.config.get<boolean>('server.response.compression.enabled')) {
+      this.serverInstance.use(compression(this.config.get<compression.CompressionFilter>('server.response.compression.options')));
+    }
     this.serverInstance.use(express.json(this.config.get<bodyParser.Options>('server.request.payload')));
 
     const ignorePathRegex = new RegExp(`^${this.config.get<string>('openapiConfig.basePath')}/.*`, 'i');
