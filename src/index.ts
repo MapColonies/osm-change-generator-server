30947a35ce9e0443558d956d21530477bf1cd2a1
melancholiai <57397441+melancholiai@users.noreply.github.com>
2022-05-22 16:48:53 +0300

    refactor: logging objects (#68)
    * refactor: logging objects
    
    * ci: updated workflow
    
    * fix(deps): generated new package-lock

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,18 @@ import { createServer } from 'http';
 import { createTerminus } from '@godaddy/terminus';
 import { Logger } from '@map-colonies/js-logger';
 import { container } from 'tsyringe';
-import { get } from 'config';
+import config from 'config';
 import { DEFAULT_SERVER_PORT, Services } from './common/constants';
 import { IServerConfig } from './common/interfaces';
 import { getApp } from './app';
 
-const serverConfig = get<IServerConfig>('server');
+const serverConfig = config.get<IServerConfig>('server');
 const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
 const app = getApp();
 
 const logger = container.resolve<Logger>(Services.LOGGER);
 const stubHealthcheck = async (): Promise<void> => Promise.resolve();
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthcheck }, onSignal: container.resolve('onSignal') });
 
 server.listen(port, () => {
