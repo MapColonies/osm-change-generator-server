d50bee532df87a9310db3c25265cd9d56fac881b
NatalieShaked <shakednatalie@gmail.com>
2023-07-27 09:51:32 +0300

    refactor: removed redundant parseInt

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,10 @@ import { Logger } from '@map-colonies/js-logger';
 import { container } from 'tsyringe';
 import config from 'config';
 import { DEFAULT_SERVER_PORT, SERVICES } from './common/constants';
-import { IServerConfig } from './common/interfaces';
 import { getApp } from './app';
 
-const serverConfig = config.get<IServerConfig>('server');
-const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
+const port: number = config.get<number>('server.port') || DEFAULT_SERVER_PORT;
+
 const app = getApp();
 
 const logger = container.resolve<Logger>(SERVICES.LOGGER);
