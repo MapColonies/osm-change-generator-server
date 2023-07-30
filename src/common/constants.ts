522d43033602f4ab778ce4e699e29f8d5fc1d3ab
NatalieShaked <shakednatalie@gmail.com>
2023-07-30 11:49:30 +0300

    style: fixed ESLint errors

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -6,6 +6,7 @@ export const DEFAULT_SERVER_PORT = 80;
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/, /^.*\/metrics.*/];
 
+/* eslint-disable @typescript-eslint/naming-convention */
 export const SERVICES: Record<string, symbol> = {
   LOGGER: Symbol('Logger'),
   CONFIG: Symbol('Config'),
